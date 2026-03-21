f6563df629b38c4e88e8d582cc6cd436d2bf796e
RedEmber-hub <redember38@gmail.com>
2026-03-22 00:59:39 +0300

    add prompt, confirm, if/else

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-let title = "GloAcademyProject";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 25000;
-let rollback = 99;
-let fullPrice = 1000000000;
-let adaptive = true;
+alert('Привет!');
+console.log('Привет!');
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?(первый вариант)");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?(второй вариант)");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let rollback = 9;
+let rollbackAmount = fullPrice * (rollback / 100);
+let servicePercentPrice = fullPrice - rollbackAmount;
+
+if(fullPrice >= 30000){
+    console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000){
+    console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+    console.log("Скидка не предусмотрена");
+} else {
+    console.log("Что то пошло не так");
+}
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
 console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани, стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
 console.log(screens.toLowerCase().split(", "));
-console.log(`Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`);
-
-alert('Привет!');
-console.log('Привет!');
+console.log(`Процент отката посреднику за работу ${rollbackAmount}`);
+console.log(servicePercentPrice);
